eef4ab8ed5aa8743fb08e4c61c47d52319babf5e
Jeff <jeffhernandez@nypl.org>
2022-11-19 20:28:59 -0500

    delete functionality is working

--- squirrelDB/squirrelDB/urls.py
+++ squirrelDB/squirrelDB/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('create', views.create, name='create'),
+    path('delete', views.delete, name='delete'),
     path('view-all', views.view_all, name='view-all'),
 ]
+
+    
--- squirrelDB/squirrelDB/views.py
+++ squirrelDB/squirrelDB/views.py
@@ -1,13 +1,9 @@
 import json
 from google.cloud import bigquery
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 client = bigquery.Client()
 
-def home(request):
-
-    return render(request, "home.html")
-    
 def view_all(request):
     context = {}
     row_dict = {}
@@ -19,7 +15,7 @@ def view_all(request):
         
         """
     )
-    results = query_job.result()  # Waits for job to complete.
+    results = query_job.result()  
     for row in results:
         row_dict["ID"] = row["ID"]
         row_dict["Above_Ground"] = row["Above_Ground"]
@@ -46,9 +42,108 @@ def view_all(request):
         row_dict["Special_Notes"] = row["Special_Notes"]
         row_copy = row_dict.copy()
         res.append(row_copy)
-    
-    print(res[2])
+
     context["results"] = res
-    # print(result)
-    # print(results.num_results)
+
     return render(request, "results.html", context)
+
+def create(request):
+    if request.method=="GET":
+        return render(request, "create.html") 
+    elif request.method=="POST":
+        # if post save in db and send of to view all, including the newest entry
+        id = request.POST.get("ID")
+        Above_Ground = request.POST.get("Above_Ground")
+        Specific_Location = request.POST.get("Specific_Location")
+        Running = request.POST.get("Running")
+        Chasing = request.POST.get("Chasing")
+        Climbing = request.POST.get("Climbing")
+        Eating = request.POST.get("Eating")
+        Foraging = request.POST.get("Foraging")
+        Kuks = request.POST.get("Kuks")
+        Quaas = request.POST.get("Quaas")
+        Moans = request.POST.get("Moans")
+        Tail_Flags = request.POST.get("Tail_Flags")
+        Tail_Twitch = request.POST.get("Tail_Twitch")
+        Approach = request.POST.get("Approach")
+        Runs_Away = request.POST.get("Runs_Away")
+        Indifferent = request.POST.get("Indifferent")
+        Hectare = request.POST.get("Hectare")
+        Date = request.POST.get("Date")
+        Shift = request.POST.get("Shift")
+        Age = request.POST.get("Age")
+        Primary_Color = request.POST.get("Primary_Color")
+        Highlight_Color = request.POST.get("Highlight_Color")
+        Special_Notes = request.POST.get("Special_Notes")
+
+        rows_to_insert = [
+            {   "id": id,
+                "Above_Ground": Above_Ground, 
+                "specific_location": Specific_Location ,
+                "running": Running,
+                "chasing": Chasing,
+                "climbing": Climbing,
+                "eating": Eating,
+                "foraging": Foraging,
+                "kuks": Kuks,
+                "quaas": Quaas,
+                "moans": Moans,
+                "tail_flags": Tail_Flags,
+                "tail_twitch": Tail_Twitch,
+                "approach": Approach,
+                "runs_away": Runs_Away,
+                "indifferent": Indifferent,
+                "hectare": Hectare,
+                "date": Date,
+                "shift": Shift,
+                "age": Age,
+                "primary_color": Primary_Color,
+                "highlight_color": Highlight_Color,
+                "special_notes": Special_Notes
+            }
+        ]
+
+        errors = client.insert_rows_json(
+            "cisc3140-368419.Squirrel_Sightings.SquirrelDB", rows_to_insert, row_ids=[None] * len(rows_to_insert)
+        ) 
+
+        if errors == []:
+            print("New rows have been added.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
+        return redirect('view-all')
+
+def delete(request):
+    if request.method=="GET":
+        return render(request, "delete.html") 
+    elif request.method=="POST":
+        id = request.POST.get("ID")
+        query = """
+        DELETE FROM `cisc3140-368419.Squirrel_Sightings.SquirrelDB` WHERE ID = @id;
+        """
+
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("id", "INTEGER", id),
+            ]
+        )
+        client.query(query, job_config=job_config) 
+        return view_all(request)
+
+def update(request):
+    if request.method=="GET":
+        return render(request, "delete.html") 
+    elif request.method=="POST":
+        id = request.POST.get("ID")
+        query = """
+        DELETE FROM `cisc3140-368419.Squirrel_Sightings.SquirrelDB` WHERE ID = @id;
+        """
+
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("id", "INTEGER", id),
+            ]
+        )
+        client.query(query, job_config=job_config) 
+        return view_all(request)
+    return view_all(request)
